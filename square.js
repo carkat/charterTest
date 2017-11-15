8bd78ff07b584e0c9b7b7aad53496c68195fc06c
nanthil <macks.smart@hotmail.com>
2017-11-15 12:03:32 -0700

    refactoring

--- square.js
+++ square.js
@@ -5,30 +5,24 @@
 //please note, I don't like the use of the counter incrementing here, but given the time constraints, I used the most convenient solution
 //I also don't like all the mutation with keys.splice
 //a better solution for both instances would be an iterator over an iterable object, calling ".next" each recursive iteration
-let generateColorsForShapes = (shapes, keys = Object.keys(shapes), colorCount = 0) =>  {
+//should make keys an iterable and colors iterable as well
+let generateColorsForShapes = (
+    shapes, 
+    keys = Object.keys(shapes), 
+    colorCount = 0
+) =>  {
     //"this" shape for "this" recursive iteration
-    let shape = keys[0]
-    let neighbors = shapes[shape].neighbors.split(',')
-    let neighborCount = neighbors.length
-    let tempColors = colors
-
-
-    //should remove neighboring colors from the list of colors
-    for(neighbor of neighbors){
-        let aNeighbor = shapes[neighbor]
-        if(aNeighbor.color !== undefined)
-            tempColors = tempColors.filter(color => color !==  aNeighbor.color)
-    }
+    const shape                    = keys[0]
+    const neighbors                = shapes[shape].neighbors.split(',')
+    const neighborCount            = neighbors.length
+    const removedNeighboringColors = colors.filter(color => !neighbors.map(n => shapes[n].color).includes(color))
 
     //set the color based on the number of neighbors
-    const ifCountIsGreaterThanThreeUniqueColor = neighborCount > 3 
-    ? shapes[shape].color = largeNeighborCountReservedColor
-    //selects a color from the filtered list of colors and uses the modulus operator to select a color within range
-    : shapes[shape].color = tempColors[colorCount++ % tempColors.length]
+    shapes[shape].color = removedNeighboringColors[colorCount++ % removedNeighboringColors.length]
 
     //"pop front", remove the first element from keys and recurse if there are any keys left
-    keys.splice(0,1)
-    return keys.length === 0 ? shapes : generateColorsForShapes(shapes, keys, colorCount)
+    const keysPopFront = keys.slice(1,keys.length)
+    return keysPopFront.length === 0 ? shapes : generateColorsForShapes(shapes, keysPopFront, colorCount)
 }
 
 //default data
@@ -55,8 +49,7 @@ let shapes = {
 }
 
 //default color data
-let colors = ['red', 'green','blue']
-let largeNeighborCountReservedColor = 'yellow'
+let colors = ['red', 'green','blue','yellow']
 
 //set the color property on shapes then print the result for demonstration
 const shapesWithColors = generateColorsForShapes(shapes)
